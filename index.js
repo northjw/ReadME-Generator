74bdec2d7f4c1c446ba10fdd3a0ea94aec12aa56
mini pony <northjw1@gmail.com>
2020-10-08 12:00:39 -0400

    troubleshooting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
 
 // array of questions for user
