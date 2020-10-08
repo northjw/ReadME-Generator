9881240dcd322bbda9a9b9757cc4db29cf7d6a5c
mini pony <northjw1@gmail.com>
2020-10-08 11:57:05 -0400

    editing layout

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ReadMe Generator
-
+undefined
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,39 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title};
+   `### Welcome to my ${data.title}
   
-Description
+#--- Description ---#
 ${data.title};
 
 
-Table of Contents (Optional)
+#--- Table of Contents ---#
 If your README is very long, add a table of contents to make it easy for users to find what they need.
 
-Installation
+*Installation
 ${data.Installation};
 
-Usage
+*Usage
 ${data.Usage};
 
-Credits
+*Credits
 ${data.Credits};
 
-License
+*License
 ${data.License};
 
 
-🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
 
-Badges
-badmath
 
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by shields.io. You may not understand what they all represent now, but you will in time.
+ #--- Badges ---#
+  ![badmath](https://img.shields.io/badge/license-${data.license}-green)
 
-Contributing
-If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The Contributor Covenant is an industry standard, but you can always write your own.
 
-Tests
-Go the extra mile and write tests for your application. Then provide examples on how to run them.
+
+#--- Tests ---#
+
+  ${data.test_instruct}
 
 `;
 }
